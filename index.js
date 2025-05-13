1f2f4c5aaf7387a17a3fbd89ff5a95c779871bee
Kaua <kaua1floripa@gmail.com>
2025-05-13 14:57:13 -0300

    criação da api rest com express

--- index.js
+++ index.js
@@ -9,11 +9,27 @@ app.listen(PORT, () =>
 {
   console.log(`Porta do servidor ${PORT}`);
 });
+const fs = require('fs');
+
+app.get('/logs/:id', (req, res) => 
+{
+  const idProcurado = req.params.id;
+  const logs = fs.readFileSync('logs.txt', 'utf-8');
+
+  const logEncontrado = logs.find(log => log.startsWith(idProcurado));
+  if (logEncontrado) 
+  {
+    return res.status(200).json({ log: logEncontrado });
+  } else 
+  {
+    return res.status(404).json({ erro: 'Log não encontrado.' });
+  }
+});
 
 app.post('/logs', (req, res) => 
 {
   const { nome } = req.body;
-  if (!nome) return res.status(400).json({ erro: 'Informe o nome' });
+  if (!nome) return res.status(400).json({ erro: 'Digite o nome' });
 
   const id = registrarLog(nome);
   res.status(201).json({ mensagem: 'Log registrado.', id });
