fecd70bab11a42a8eb74db7417ccdd4d7b17b67f
Kaua <kaua1floripa@gmail.com>
2025-05-13 15:00:50 -0300

    log

--- script.js
+++ script.js
@@ -11,4 +11,15 @@ function registrarLog(nomeAluno) {
   } else {
     return res.status(404).json({ erro: 'Log não encontrado.' });
   }
+}
+app.get('/logs/:id', (req, res) => {
+  const idProcurado = req.params.id;
+  const logs = fs.readFileSync('logs.txt', 'utf-8').split('\n');
+
+  const logEncontrado = logs.find(log => log.startsWith(idProcurado));
+  if (logEncontrado) {
+    return res.status(200).json({ log: logEncontrado });
+  } else {
+    return res.status(404).json({ erro: 'Log não encontrado.' });
+  }
 });
